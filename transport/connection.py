fdae57d136a420c91b26817708157b557ec79818
Lucho <sportelliluciano@gmail.com>
2022-05-29 18:41:09 -0300

    Update connection.py

--- transport/connection.py
+++ transport/connection.py
@@ -2,9 +2,24 @@ class Connection:
     def __init__(self, controller: RdpController):
         self._controller = controller
     
-    @property
-    def controller(self):
-        return self._controller
+    def on_tick(self, current_time):
+        """
+        Called by the protocol in regular periods of time
+        """
+        self._controller.on_tick(current_time)
+    
+    def on_data_received(self, segment):
+        """
+        Called by the protocol when a new segment containing data has 
+        been received.
+        """
+        self._controller.on_data_received(segment)
+    
+    def on_ack_received(self, segment):
+        """
+        Called by the protocol when a new ACK has been received.
+        """
+        self._controller.on_ack_received(segment)
    
     @staticmethod
     def connect(address, port):
